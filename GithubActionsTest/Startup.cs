b4404f7358457f1f48f82cf023ca0db2ff8916b6
Suhas <suhas.2ab@gmail.com>
2020-12-02 11:12:49 +0530

    Update Startup.cs

--- GithubActionsTest/Startup.cs
+++ GithubActionsTest/Startup.cs
@@ -32,7 +32,7 @@ namespace GithubActionsTest
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World! Changed message to test CI");
                 });
             });
         }
